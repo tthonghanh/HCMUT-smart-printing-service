71f41ba845eecbf880992bc607ea797154ffcc8b
Tran Thi Hong Hanh <tthh260904@gmail.com>
2024-12-11 11:37:44 +0700

    update report, source structure

---
+++ src/Controller/config.py
@@ -0,0 +1,15 @@
+STUDENT_ACCOUNT = {
+    'tranthienphu': 'password123'
+}
+MANAGER_ACCOUNT = {
+    'spso': 'admin'
+}
+
+
+USER_CONFIG = {
+    'tranthienphu': {
+        'remaining_pages': 100,
+        'remaining_balance': 100,
+        
+    }
+}
